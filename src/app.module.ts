80b12836031907ffdd324f181357f527ddb31d5d
Jiang JunYang <littlesheep.code@hotmail.com>
2022-03-23 11:00:35 +0800

    add Layout and Index page

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { StateController } from './state/state.controller';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { UsersModule } from './authorization/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { IndexController } from './index.controller';
 
 @Module({
   imports: [AuthorizationModule, UsersModule, PrismaModule],
-  controllers: [StateController],
+  controllers: [StateController, IndexController],
   providers: [],
 })
 export class AppModule {
