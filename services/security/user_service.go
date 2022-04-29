7416fa12624c2dd9590cf1e66b88587a706a7f90
Jiang JunYang <littlesheep.code@hotmail.com>
2022-04-29 23:08:45 +0800

    ready: add verify service

--- services/security/user_service.go
+++ services/security/user_service.go
@@ -8,20 +8,21 @@ import (
 
 	"github.com/google/uuid"
 	"gorm.io/datatypes"
+	"gorm.io/gorm"
 )
 
 type UserService struct {
+	connection *gorm.DB
 	passwordService *services.PasswordService
 }
 
 func NewUserService() *UserService {
 	service := &UserService{
+		connection: datasource.GetConnection(),
 		passwordService: &services.PasswordService{},
 	}
 	return service
 }
-
-var connection = datasource.GetConnection()
 
 func (self *UserService) CreateUser(user models.User, force bool) bool {
 	userUuid := uuid.New()
@@ -31,18 +32,18 @@ func (self *UserService) CreateUser(user models.User, force bool) bool {
 	if !force {
 		model := models.User{ID: userUuid.String(), Username: user.Username, Password: userPassword, Email: user.Email, Attributes: datatypes.JSON([]byte(`{}`))}
 		verify := models.VerifyCode{UID: userUuid.String(), Code: strings.ToUpper(codeContent.String()[:6]), Type: "active"}
-		err := connection.Create(&model).Error
+		err := self.connection.Create(&model).Error
 		if err != nil {
 			return false
 		} else {
-			err = connection.Create(&verify).Error
+			err = self.connection.Create(&verify).Error
 			if err != nil {
 				return false
 			}
 		}
 	} else {
 		model := models.User{ID: userUuid.String(), Username: user.Username, Password: userPassword, Email: user.Email, Attributes: datatypes.JSON([]byte(`{}`)), IsActive: true}
-		err := connection.Create(&model).Error
+		err := self.connection.Create(&model).Error
 		if err != nil {
 			return false
 		}
@@ -51,6 +52,18 @@ func (self *UserService) CreateUser(user models.User, force bool) bool {
 	return true
 }
 
-func (self *UserService) ActiveUser(code string) {
+func (self *UserService) ActiveUser(code string) bool {
+	var err error
 
+	var verifyCode models.VerifyCode
+	var verifyUser models.User
+	if err = self.connection.Where(models.VerifyCode{Code: code}).First(&verifyCode).Error; err != nil {
+    return false
+	} else {
+		if err = self.connection.Where(models.User{ID: verifyCode.UID}).First(&verifyUser).Error; err != nil {
+			return false
+		}
+	}
+
+	return true
 }
