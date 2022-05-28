f53483a8910ca5930f408836b78bb80a5e77fff3
Jiang JunYang <littlesheep.code@hotmail.com>
2022-05-29 00:22:20 +0800

    feat: opeartion open and commit

--- src/controllers/operations.controller.ts
+++ src/controllers/operations.controller.ts
@@ -18,7 +18,10 @@ export class OperationController {
       orderBy: { created_at: "desc" },
       skip: skip,
       take: take,
-      where: ignore === "yes" ? {} : { conditions: { path: "$.level", lt: request.user.level } },
+      where: Object.assign(
+        { status: "published" },
+        ignore === "yes" ? {} : { conditions: { path: "$.level", lte: request.user.level } },
+      ),
     });
     for (const item of response) {
       for (const judge of item.data["judgement"]) {
@@ -75,5 +78,97 @@ export class OperationController {
       },
       Response: response,
     });
+  }
+
+  @Get("/detail")
+  async getOperationDetail(@Query("id") id: string) {
+    const { operation, records } = await this.operationService.getOperationDetail(id);
+    return {
+      Status: {
+        Code: "OK",
+        Message: "Successfully fetch operation detail",
+      },
+      Response: {
+        Operation: operation,
+        Records: records,
+      },
+    };
+  }
+
+  @Post("/start")
+  @HttpCode(200)
+  async startOperation(@Request() request: any, @Res() res: any, @Body("id") id: string) {
+    const response = await this.operationService.startOperation(request.user.id, id);
+    if (response == null) {
+      res.status(400).send({
+        Status: {
+          Code: "DATA_ERROR",
+          CodeDetail: "DISSATISFY_THE_CONDITIONS",
+          Message: "Cannot start operation, dissatisfy the operation start condition",
+        },
+        Response: response,
+      });
+    } else {
+      res.send({
+        Status: {
+          Code: "OK",
+        },
+        Response: response,
+      });
+    }
+  }
+
+  @Post("/commit")
+  @HttpCode(200)
+  async commitOperation(
+    @Request() request: any,
+    @Res() res: any,
+    @Body("id") id: number,
+    @Body("code") code: string,
+    @Query("runtime") runtime: string,
+    @Query("runtimeVersion") runtimeVersion: string,
+  ) {
+    if (code.replace(" ", "") === "") {
+      res.status(400).send({
+        Status: {
+          Code: "DATA_ERROR",
+          CodeDetail: "CODE_CANNOT_AS_NULL",
+          Message: "Cannot commit operation, code is null.",
+        },
+        Response: null,
+      });
+      return;
+    }
+    const response = await this.operationService.commitOperation(id, code, runtime, runtimeVersion);
+    if (response.Message === "RUNTIME_NOT_AVAILABLE") {
+      res.status(400).send({
+        Status: {
+          Code: "DATA_ERROR",
+          CodeDetail: response.Message,
+          Message:
+            "Cannot commit operation, runtime not available, please change a language or contact the administrator.",
+        },
+        Response: null,
+      });
+    } else if (response.Message === "RECORD_NOT_FOUND") {
+      res.status(400).send({
+        Status: {
+          Code: "DATA_ERROR",
+          CodeDetail: response.Message,
+          Message: "Cannot commit operation, record not found, please start operation first.",
+        },
+        Response: null,
+      });
+    } else {
+      res.send({
+        Status: {
+          Code: response.Finished ? "FINISHED" : "OK",
+          Message: response.Finished
+            ? "Current operation is finish, operation automatic closed."
+            : "Successfully commit operation.",
+        },
+        Response: response,
+      });
+    }
   }
 }
--- src/services/operations.service.ts
+++ src/services/operations.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { v4 as uuidv4 } from "uuid";
+import { operations as OperationModel, records_operation as RecordOperationModel } from "@prisma/client";
+import axios from "axios";
 
 @Injectable()
 export class OperationService {
@@ -15,5 +17,91 @@ export class OperationService {
         publisher: publisher,
       },
     });
+  }
+
+  async getOperationDetail(id: string): Promise<{ operation: OperationModel; records: RecordOperationModel[] }> {
+    const operation = await this.prisma.operations.findFirst({ where: { id: id, status: "published" } });
+    const records = await this.prisma.records_operation.findMany({ where: { operation: id } });
+    return { operation, records };
+  }
+
+  async startOperation(uid: string, id: string) {
+    const user = await this.prisma.users.findUnique({ where: { id: uid } });
+    const operation = await this.prisma.operations.findUnique({ where: { id: id } });
+    const progress = await this.prisma.records_operation.count({
+      where: { uid: user.id, status: "finished", in_progress: true },
+    });
+
+    if (user.level < operation.conditions["level"]) {
+      return null;
+    } else if (progress < operation.conditions["progress"]) {
+      return null;
+    } else {
+      return await this.prisma.records_operation.create({
+        data: { uid: user.id, operation: operation.id, data: [], status: "working" },
+      });
+    }
+  }
+
+  async commitOperation(recordId: number, code: string, runtime: string, runtimeVersion: string, memory = -1) {
+    const record = await this.prisma.records_operation.findFirst({ where: { id: recordId, status: "working" } });
+    if (record == null) {
+      return { Message: "RECORD_NOT_FOUND" };
+    } else {
+      memory = record.data["memory"] ? record.data["memory"] : memory;
+    }
+    const operation = await this.prisma.operations.findUnique({ where: { id: record.operation } });
+
+    let isFinished = false;
+    const isDuplicated =
+      (await this.prisma.records_operation.count({ where: { uid: record.uid, status: "finished" } })) === 0
+        ? false
+        : true;
+    const mark = [];
+    const result = [];
+    for (const judge of operation.data["judgement"]) {
+      const response = await axios
+        .create({ validateStatus: () => true })
+        .post(process.env.RUNTIME_URL + "/api/v2/execute", {
+          language: runtime,
+          version: runtimeVersion,
+          files: [{ name: "main", content: code }],
+          stdin: judge["stdin"] ? judge["stdin"] : "",
+          args: judge["args"] ? judge["args"] : [],
+          compile_timeout: 10000,
+          run_timeout: 3000,
+          compile_memory_limit: -1,
+          run_memory_limit: memory,
+        });
+      if (response.status === 200) {
+        const stdout = response.data.run["stdout"];
+        isFinished = (stdout.endsWith("\n") ? stdout.slice(0, stdout.length - 1) : stdout) === judge["stdout"];
+        mark.push(isFinished);
+        result.push(response.data);
+        if (!isFinished) {
+          break;
+        }
+      } else {
+        return { Message: "RUNTIME_NOT_AVAILABLE" };
+      }
+    }
+    await this.prisma.records_operation.update({
+      where: { id: recordId },
+      data: {
+        data: (record.data as Array<object>).concat({
+          mark: mark,
+          result: result,
+          code: code,
+        }),
+        status: isFinished ? "finished" : "working",
+        in_progress: !isDuplicated,
+      },
+    });
+    return {
+      Failed: !isFinished,
+      Finished: isFinished,
+      Mark: mark,
+      Result: result,
+    };
   }
 }
